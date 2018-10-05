27b0c172ea1c326244b0bf9f28f5656b79c1108a
Damoone <ambroise1.durand@gmail.com>
2018-10-05 23:14:07 +0200

    add obstacles and couloirs

---
+++ Over9000Kart/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	public float speed = 10;
+
+	 Vector2 couloir;
+
+	Camera camera;
+
+
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		goLeft();
+		
+	}
+
+	void goLeft()
+	{
+		transform.Translate(Vector2.left * Time.deltaTime * speed);
+	}
+
+	void destroyObject() 
+	{
+		Destroy(this.gameObject);
+	}
+
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.tag == ("MainCamera")) {
+			destroyObject();
+		}
+	}
+}
---
+++ Over9000Kart/Assets/couloirs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class couloirs : MonoBehaviour {
+
+	// Use this for initialization
+	public List<Vector3> couloirsList;
+	public int numberOfCorridor = 4;
+	public GameObject background;
+	private float lengthCorridor;
+	private float corridorPosition;
+	private float distanceCorridor = 0;
+	public GameObject test;
+	private float firstCorridorDistance;
+
+	void Start () {
+		lengthCorridor = background.transform.localScale.y/ numberOfCorridor;
+		distanceCorridor = (background.transform.position.y - background.transform.localScale.y / 2) + lengthCorridor / 2;
+		firstCorridorDistance = distanceCorridor;
+
+		for (int i = 0; i< numberOfCorridor; i ++) {
+			couloirsList.Add((new Vector3(background.transform.position.x, distanceCorridor,-1)));
+			distanceCorridor = distanceCorridor + lengthCorridor;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
