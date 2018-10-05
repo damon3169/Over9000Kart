5f06507f88ce8cc3556965607b470c895c0d33d3
Damoone <ambroise1.durand@gmail.com>
2018-10-06 00:46:33 +0200

    add the random obstacles to the game

--- Over9000Kart/Assets/Scripts/GameManager.cs
+++ Over9000Kart/Assets/Scripts/GameManager.cs
@@ -7,7 +7,18 @@ public class GameManager : MonoBehaviour {
 
     List<Ship> listShip; // liste des vaisseaux dans une partie
     public GameObject GameObjectUIManager;
-    UiManager uiManager; 
+    UiManager uiManager;
+	public couloirs couloirs;
+	public float timerObstaclesRange = 10;
+	public float timerObstacles;
+	private float timerObstaclesBegin;
+	private int numberObstacle;
+	private List<int> usedCouloirs;
+	public GameObject obstacle;
+	private int SpawnIn;
+	private bool isObstacleSpawn;
+	public GameObject background;
+
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +29,11 @@ public class GameManager : MonoBehaviour {
         {
             listShip.Add(ship.GetComponent<Ship>());
         }
+		timerObstaclesBegin = 0;
+		timerObstacles = Random.Range(1, timerObstaclesRange);
+		usedCouloirs = new List<int>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		for(int i=0; i<listShip.Count; i++)
@@ -34,5 +48,29 @@ public class GameManager : MonoBehaviour {
                     break;
             }
         }
+
+		if (Time.time > timerObstaclesBegin + timerObstacles) {
+			timerObstacles = Random.Range(1, timerObstaclesRange+1);
+			numberObstacle = Random.Range(1, couloirs.numberOfCorridor);
+			timerObstaclesBegin = Time.time;
+			usedCouloirs.Clear();
+			Debug.Log(numberObstacle);
+			for (int i = 0; i< numberObstacle; i++)
+			{
+				isObstacleSpawn = false;
+				while (!isObstacleSpawn) { 
+					 SpawnIn= Random.Range(0, couloirs.numberOfCorridor);
+					if (usedCouloirs == null || !usedCouloirs.Contains(SpawnIn))
+					{
+						usedCouloirs.Add(SpawnIn);
+						GameObject obst = Instantiate(obstacle);
+						obst.transform.position= new Vector3(background.transform.position.x+background.transform.localScale.x/2 + 2, couloirs.couloirsList[SpawnIn].y, couloirs.couloirsList[SpawnIn].z);
+						isObstacleSpawn = true;
+					}
+				}
+			}
+		}
+
+
 	}
 }
