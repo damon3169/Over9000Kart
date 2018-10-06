7f95192e20585a7971ead23cbee8ae080111f6de
Damoone <ambroise1.durand@gmail.com>
2018-10-06 12:46:26 +0200

    Revert "direction wip"
    This reverts commit e9209e28bb5a24c081feeeebf5abe32ea45e621a.

--- Over9000Kart/Assets/Scripts/Ship.cs
+++ Over9000Kart/Assets/Scripts/Ship.cs
@@ -8,44 +8,26 @@ public class Ship : MonoBehaviour {
     public Vector2 position; // position x y du vaisseau
     public int idJoueur; // numéro du joueur controlant le vaisseau
     string controleurJoueur; // nom du bouton correspondant au numéro de joueur
-	private int actualCorridor;
-	public couloirs corridor;
-
 
 	// Use this for initialization
 	void Start () {
         speed = -0.1f; ; // vitesse de base du vaisseau
-		switch (idJoueur) // 
+        switch(idJoueur) // 
         {
             case 1:
                 controleurJoueur = "Player1";
-
-
-				break;
+                break;
             case 2:
                 controleurJoueur = "Player2";
-				break;
+                break;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown(controleurJoueur+"_SpeedUp") && speed<3) speed += 0.2f;
-		if (Input.GetButtonDown(controleurJoueur + "_ChangeCorridor") )
-		{
-			if (Input.GetAxis(controleurJoueur + "_ChangeCorridor") < 0)
-			{
-				Debug.Log(Input.GetAxis(controleurJoueur + "_ChangeCorridor"));
-			}
-		}
-
-		if (speed > -1) speed -= 0.03f; // frein naturel
+        if (speed > -1) speed -= 0.03f; // frein naturel
         
         if (speed > 0) gameObject.transform.Translate(Time.deltaTime*speed, 0, 0);
-	}
-
-	void setActualCorridor(int corridor)
-	{
-		actualCorridor = corridor;
 	}
 }
