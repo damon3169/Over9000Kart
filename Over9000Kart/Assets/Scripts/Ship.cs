4e4395951934f6c02fa25cf359e5ca86584585e6
Damoone <ambroise1.durand@gmail.com>
2018-10-06 13:44:24 +0200

    Add direction

--- Over9000Kart/Assets/Scripts/Ship.cs
+++ Over9000Kart/Assets/Scripts/Ship.cs
@@ -2,41 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ship : MonoBehaviour {
+public class Ship : MonoBehaviour
+{
 
-    public float speed; // vitesse du vaisseau
-    public int score; // score de vitesse du vaisseau 
-    //public Vector2 position; // position x y du vaisseau
-    public int idJoueur; // numéro du joueur controlant le vaisseau
-    string controleurJoueur; // nom du bouton correspondant au numéro de joueur
-    public float speedMin;
-    GameManager gameManager;
+	public float speed; // vitesse du vaisseau
+	public Vector2 position; // position x y du vaisseau
+	public int idJoueur; // numéro du joueur controlant le vaisseau
+	string controleurJoueur; // nom du bouton correspondant au numéro de joueur
+	private int actualCorridor;
+	public couloirs corridor;
+
 
 	// Use this for initialization
-	void Start () {
-        //gameManager = new GameManager();
-        //transform.position = new Vector3(gameManager.getCameraWidth() % 25, transform.position.y, transform.position.z);
-        score = 0;
-        speed = 0f; ; // vitesse de base du vaisseau
-        switch(idJoueur) // 
-        {
-            case 1:
-                controleurJoueur = "Player1";
-                break;
-            case 2:
-                controleurJoueur = "Player2";
-                break;
-        }
-        Debug.Log(controleurJoueur);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown(controleurJoueur+"_SpeedUp") && speed<3) speed += 0.2f;
-        if (speed > -1) speed -= 0.02f; // frein naturel
-        //score = gameObject.transform.position.x;
+	void Start()
+	{
+		speed = -0.1f; ; // vitesse de base du vaisseau
+		switch (idJoueur) // 
+		{
+			case 1:
+				controleurJoueur = "Player1";
 
-        if (speed > speedMin) gameObject.transform.Translate(Time.deltaTime * speed, 0, 0);
-        else speed = speedMin+0.1f;
+
+				break;
+			case 2:
+				controleurJoueur = "Player2";
+				break;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown(controleurJoueur + "_SpeedUp") && speed < 3) speed += 0.2f;
+		if (Input.GetButtonDown(controleurJoueur + "_ChangeCorridor"))
+		{
+			if (Input.GetAxis(controleurJoueur + "_ChangeCorridor") < 0)
+			{
+				if (actualCorridor > 0)
+				{
+					setActualCorridor(actualCorridor - 1);
+					this.transform.position = new Vector3(transform.position.x, corridor.couloirsList[actualCorridor].y, transform.position.z);
+				}
+
+				if (actualCorridor > 0)
+				{
+
+				}
+			}
+		}
+
+		if (speed > -1) speed -= 0.03f; // frein naturel
+
+		if (speed > 0) gameObject.transform.Translate(Time.deltaTime * speed, 0, 0);
+	}
+
+	public void setActualCorridor(int corridor)
+	{
+		actualCorridor = corridor;
 	}
 }
