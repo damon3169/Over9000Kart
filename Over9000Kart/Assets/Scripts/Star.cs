007efabbde1923e095ba86e09946c971226ff48d
floalazais <flo.alazais@gmail.com>
2018-10-06 14:15:39 +0200

    star script

---
+++ Over9000Kart/Assets/Scripts/Star.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+
+        if (transform.position.x + transform.localScale.x / 2 < GameManager.instance.cam.transform.position.x - GameManager.instance.getCameraWidth() / 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
