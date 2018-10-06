a3ee3a99262fa8b00bace20e9d97140831c797a2
floalazais <flo.alazais@gmail.com>
2018-10-06 14:18:07 +0200

    removed redundant instructions in obstacle

--- Over9000Kart/Assets/Scripts/Obstacle.cs
+++ Over9000Kart/Assets/Scripts/Obstacle.cs
@@ -2,20 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle : MonoBehaviour {
-
-    public float speed;
-
+public class Obstacle : Star {
 	// Use this for initialization
 	void Start () {}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector2.left * Time.deltaTime * speed);
-    
-		if (transform.position.x + transform.localScale.x / 2 < GameManager.instance.cam.transform.position.x - GameManager.instance.getCameraWidth() / 2)
-        {
-            Destroy(this.gameObject);
-        }
+        
 	}
 }
