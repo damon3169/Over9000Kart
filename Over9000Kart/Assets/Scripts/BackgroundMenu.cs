47c1b781f08e20128d532b7618fbbf2ef4cb615d
helarion <axel.malherbe0@gmail.com>
2018-10-07 20:37:40 +0200

    Menu
    Mise du background étoilé en menu et explication (code dégueulasse mais ça marche) les boutons sont posés

---
+++ Over9000Kart/Assets/Scripts/BackgroundMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMenu : MonoBehaviour {
+    // stars et background
+    public GameObject star;
+    float timeSinceLastStarGenerated = 0.0f;
+    float starGenerationCooldown = 0.01f;
+    public GameObject background;
+    public float randomDistance = 3;
+    public float starSpeed;
+    public float starSize;
+
+    public Camera cam;
+
+    private float random_width()
+    {
+        return cam.transform.position.x - cam.orthographicSize * cam.aspect + Random.Range(0.0f, cam.orthographicSize * cam.aspect * 2);
+    }
+
+    private float random_height()
+    {
+        return cam.transform.position.y - cam.orthographicSize + Random.Range(0.0f, cam.orthographicSize * 2);
+    }
+
+    // Use this for initialization
+    void Start () {
+        cam = Camera.main;
+        Star[] stars = FindObjectsOfType<Star>();
+        for (int i = 0; i < stars.Length; i++)
+        {
+            Destroy(stars[i]);
+        }
+
+        int nbStars = Random.Range(500, 2000); // nombre aléatoire d'étoiles à l'écran
+
+        for (int i = 0; i < nbStars; i++) // génération des étoiles sur le background
+        {
+            GameObject newStar = Instantiate(star);
+            newStar.transform.position = new Vector3(random_width(), random_height(), 0);
+        }
+
+        starSize = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        starSpeed = 1;
+        starSize = 0.2f;
+
+        timeSinceLastStarGenerated += Time.deltaTime;
+        if (timeSinceLastStarGenerated >= starGenerationCooldown)
+        {
+            timeSinceLastStarGenerated = 0.0f;
+            for (int i = 0; i < starSpeed; i++)
+            {
+                GameObject newStar = Instantiate(star);
+                newStar.transform.position = new Vector3(cam.transform.position.x + cam.orthographicSize * cam.aspect, random_height(), 0);
+            }
+        }
+    }
+}
---
+++ Over9000Kart/Assets/Scripts/StarMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarMenu : MonoBehaviour
+{
+
+    Camera cam;
+    // Use this for initialization
+    void Start() { cam = Camera.main; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * 1);
+
+        transform.localScale = new Vector3(0.2f, 0.1f, 0.1f);
+
+        if (transform.position.x + transform.localScale.x / 2 < cam.transform.position.x - getCameraWidth() / 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public float getCameraHeight()
+    {
+        float height = 2f * cam.orthographicSize;
+        return height;
+    }
+
+    // permet d'obtenir la taille en largeur de la caméra
+    public float getCameraWidth()
+    {
+        float width = getCameraHeight() * cam.aspect;
+        return width;
+    }
+}
